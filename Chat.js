3e909e5bff9de99171554589c954f394e2824486
Rishit-5 <rishistar05@gmail.com>
2021-11-14 14:18:16 -0600

    ignopreeeeeeee

--- Chat.js
+++ Chat.js
@@ -1,12 +1,12 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBlTVYdKivk0KrHreXwrJ6YA8VxTVHbIJs",
-    authDomain: "athlead-67a40.firebaseapp.com",
-    databaseURL: "https://athlead-67a40-default-rtdb.firebaseio.com",
-    projectId: "athlead-67a40",
-    storageBucket: "athlead-67a40.appspot.com",
-    messagingSenderId: "45769101298",
-    appId: "1:45769101298:web:5ec9ec7a1e38cb4362e56b",
-    measurementId: "G-GWLT1C4SZ0"
+const firebaseConfig = {
+    apiKey: "AIzaSyAC1nOk89qvz9-YUXwRgd-hhLzJezYhvMU",
+    authDomain: "calgaryhackathon-67df8.firebaseapp.com",
+    databaseURL: "https://calgaryhackathon-67df8-default-rtdb.firebaseio.com",
+    projectId: "calgaryhackathon-67df8",
+    storageBucket: "calgaryhackathon-67df8.appspot.com",
+    messagingSenderId: "90074114709",
+    appId: "1:90074114709:web:4650652206d4c25a46499c",
+    measurementId: "G-GPL6JPKL4P"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
