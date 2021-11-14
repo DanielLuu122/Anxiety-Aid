69d93b3db1e4f5789aeb9ba06a46f0cd7a737b87
DanielLuu122 <danluu11@shaw.ca>
2021-11-14 16:59:05 -0700

    added quotes functionality, no button yet

---
+++ Inspiration.js
@@ -0,0 +1,13 @@
+document.getElementById("quotes").onclick = function () {
+    let quotesDiv = document.getElementById("quotes");
+    fetch("https://type.fit/api/quotes")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let quote = data[Math.floor(Math.random() * 1643)]["text"];
+        console.log(quote);
+        document.getElementById("quotes").innerHTML = quote;
+      });
+  
+  };
