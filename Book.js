5253ad5fe823d92ef0486e6893690f1d88c1fe90
DanielLuu122 <danluu11@shaw.ca>
2021-11-17 21:41:10 -0700

    oopsies last push failed

--- Book.js
+++ Book.js
@@ -1,5 +1,9 @@
-
-
-document.getElementById("btn").onclick = function() {
-    document.getElementById("input").value = "";
- }
+document.getElementById("btn").onclick = function () {
+  document.getElementById("input").value = "";
+  document.getElementById("input").style.display = "none";
+  setTimeout(function(){
+    document.getElementById("input").style.display = "block";},
+    5000,
+  );
+  //document.getElementById("fire").style.display = "block";
+};
